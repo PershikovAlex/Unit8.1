86b10134a44fdacf330ad2e0d7ee6d28bc25d52a
Aleksandr Pershikov <pershikov-ai@mail.ru>
2023-11-19 16:00:39 +0500

    Add DashboardPage LoginPage VerificationPage

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -1,3 +1,22 @@
 package ru.netology.page;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import java.util.Calendar;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class LoginPage {
+    private SelenideElement loginField = $("[data-test-id=login] input");
+    private SelenideElement passwordFieled = $("[data-test-id=password] input");
+    private SelenideElement loginButton = $("[data-test-id=action-login]");
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordFieled.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+
 }
