2bc808fc8f9d5d9334818999adc9fc12450cf72e
Aleksandr Pershikov <pershikov-ai@mail.ru>
2023-11-19 16:16:48 +0500

    Add first test

--- src/test/java/ru/netology/test/AuthTest.java
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -1,4 +1,23 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.Configuration;
+import lombok.var;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class AuthTest {
+    @BeforeEach
+    void setUp() {
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void validLoginTest() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+    }
 }
